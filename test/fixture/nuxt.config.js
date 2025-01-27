20dc6ed1eaab75994d4ac1c9eedda058068cada0
Vladimir Dorogov <i@d-vantage.com>
2025-01-28 05:30:16 +0700

    feat: implementation of Nuxt Bridge support, integration with Redis

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -1,39 +1,38 @@
-module.exports = {
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
+  bridge: true,
   srcDir: __dirname,
   render: {
     resourceHints: false,
   },
-  modules: [require('../..')],
-  sitemap: [
-    {
-      path: '/sitemap.xml',
-      exclude: ['/exclude'],
-      gzip: true,
-      hostname: 'http://localhost:3000/',
-      routes: ['1/', 'child/1', { url: 'test' }],
-      filter: ({ routes }) => routes.filter((route) => route.url !== '/filtered/'),
-      xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
-      // xslUrl: 'sitemap.xsl',
-      trailingSlash: true,
-      defaults: {
-        changefreq: 'daily',
-        priority: 1,
+  modules: ['../lib/module'],
+  sitemap: {
+    redis: {
+      useForSitemap: true,
+      useGzip: true,
+      keyName: 'some-redis-key',
+      partNamespace: 'item',
+      config: {
+        host: '127.0.0.1',
+        port: 6379,
+        password: '123',
       },
     },
-    {
-      hostname: 'https://example.com/',
-      lastmod: new Date().toISOString(),
-      sitemaps: [
-        {
-          path: '/sitemap-foo.xml',
-          routes: ['foo/1', 'foo/2'],
-        },
-        {
-          hostname: 'https://yolo.com/',
-          path: '/sitemap-bar.xml',
-          routes: ['bar/1', 'bar/2'],
-        },
-      ],
-    },
-  ],
-}
+    // gzip: true,
+    path: '/sitemap.xml',
+    sitemaps: [
+      {
+        path: '/static-pages.xml',
+        lastmod: new Date().toISOString(),
+        // gzip: true,
+        exclude: ['/foo3'],
+      },
+      {
+        path: '/sitemap-foo.xml',
+        routes: ['foo/1', 'foo/2'],
+      },
+    ],
+    hostname: 'http://localhost:3000/',
+  },
+})
